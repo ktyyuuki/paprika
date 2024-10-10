40d6ba08437fc73f91d32b2cd57d09046428ad94
Katherine Droguett <ktysakura_jb.30stm@hotmail.com>
2024-10-09 21:43:38 -0300

    corrección menu duplicado (js), baja de peso imgs

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-const menu = [];
+// RENDER PRODUCTOS
+async function filtroProductos(categoria){
+    const response = await fetch("../json/menu.json");
+    const data = await response.json();
 
-// Leer json
-async function consumirJSON() {
-    if (menu.length === 0) { 
-        const response = await fetch("../json/menu.json");
-        const data = await response.json();
-        data.forEach(item => {
-            menu.push(item);
-        });
-    }
-    return menu;
+    let filtro = [];
+    data.forEach(item => {
+        if(item.category === categoria){
+            filtro.push(item);
+        }
+    });
+    // console.log(filtro);
+    return filtro;
 }
-// console.log(consumirJSON());
-// console.log(menu);
 
+async function render(cat, containerId){
+    const productosFiltrados = await filtroProductos(cat);
+    console.log(productosFiltrados); 
 
+    const container = document.getElementById(containerId);
+    let contenidoHTML = "";
 
-// Estructura menu
-function renderMenu(menu){ 
-    let productoHTML = "";
-
-    for (const item of menu) {
-        productoHTML += `<article class="col-lg-6">
+    productosFiltrados.forEach(item => {
+        contenidoHTML += `<article class="col-lg-6">
             <div class="menu-item">`
             if(item.img){
-                productoHTML += `<img src="${item.img}" width="100" height="100" alt="${item.name}" title="${item.name}">`
+                contenidoHTML += `<img src="${item.img}" width="100" height="100" alt="${item.name}" title="${item.name}">`
             }
-            productoHTML += `<div class="descripcion-menu">
+            contenidoHTML += `<div class="descripcion-menu">
                     <div class="header-menu">
                         <h3 class="titulo-menu">${item.name}</h3>
                         <p class="precio-menu">$${item.price}</p>
@@ -35,29 +35,13 @@ function renderMenu(menu){
                 </div>
             </div>
         </article>`;
-        // document.getElementById("smallplates").innerHTML = productoHTML;
-    }  
-    return productoHTML;
-}
-// renderMenu(menu);
-
-async function filtrarMenu(categoria, containerId){
-    let menu = await consumirJSON();
-    // console.log(menu);
-    const productosFiltrados = menu.filter(item => item.category == categoria);
-    console.log(productosFiltrados);
-
-    const container = document.getElementById(containerId);
-    const productosHTML = renderMenu(productosFiltrados);
-    container.innerHTML = productosHTML;
-    
+        console.log(contenidoHTML);
+        container.innerHTML = contenidoHTML;   
+    })
 }
 
-// Ejecutar la función filtrarMenu para cada categoría una sola vez
-document.addEventListener('DOMContentLoaded', () => {
-    filtrarMenu('Small plates', 'smallplates');  
-    filtrarMenu('Entrees', 'entreess');
-    filtrarMenu('Desserts', 'dessert');
-    filtrarMenu('Wine', 'wines');
-    filtrarMenu('Beer', 'beers');
-});
+render('Small plates', 'smallplates');
+render('Entrees', 'entreess');
+render('Desserts', 'dessert');
+render('Wine', 'wines');
+render('Beer', 'beers');
